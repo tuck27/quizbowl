a065c2da127dcb23bf6eaf0753ab52ffa9924fe4
Maya <mayas2744@tuckahoeschools.org>
2024-11-13 13:24:31 +0000

    third commit

--- script.js
+++ script.js
@@ -3,14 +3,30 @@ const submitButton = document.querySelector("#submit-button");
 const input = document.querySelector('#input-field');
 const questionElement = document.querySelector(".question p");
 
-function changeQuestion() {
-    const questions = [
-        "Who won the first SuperBowl?",
-        "What baseball team has won 27 world series?",
-    ]
-    changeSubmitButton.addEventListener("click", () => {
-        const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-        questionElement.textContent = randomQuestion;
+
+let questionList = [
+    "Who won the first SuperBowl?",
+    "What baseball team has won 27 world series?",
+    "What is the name of the trophy awarded to the NHL playoff champion?",
+    "What does NBA stand for?"
+]
+questionElement.textContent = questionList[0];
+changeSubmitButton.addEventListener("click", () => {
+    const randomQuestion = questionList[Math.floor(Math.random() * questionList.length)];
+    questionElement.textContent = randomQuestion;
+});
+
+let answerList = [
+    'Green Bay Packers',
+    'New York Yankees',
+    'Stanley Cup',
+    'National Basketball Association'
+]
+questions.textContent = questionList[0];
+input.textContent = answerList[0];
+
+if (input === 0){
+    submitButton.addEventListener("click", () => {
+        answerList 
     })
-};
-changeQuestion()
+}
